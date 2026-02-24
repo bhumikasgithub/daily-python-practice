9198ff5bd9ecb646d34917d18f6b17b0b7206464
Bhumika Shinde <bhumikashinde42@gmail.com>
2026-02-24 11:27:33 +0530

    Prime Number using Recursion in Python

---
+++ day13.py
@@ -0,0 +1,15 @@
+
+#find the number is prime or not
+def Prime_Number(n, i=2):
+    if n == i:
+        return True
+    elif n % i == 0:
+        return False
+    return Prime_Number(n, i + 1)
+
+
+n = 971
+if Prime_Number(n):
+    print("Yes,", n, "is Prime")
+else:
+    print("No,", n, "is not a Prime")
