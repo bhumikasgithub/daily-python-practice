d9fc237ac776d55098e030bb5add3803bdcd3da2
Bhumika Shinde <bhumikashinde42@gmail.com>
2026-03-17 09:44:16 +0530

    Find the Factorial of the Number using Recursion and iteration in Python

--- day22.py
+++ day22.py
@@ -1,8 +1,18 @@
 # find the factorial of the number using resursion 
-
+'''
 def getfact(n):
     if n==0:
         return 1
     return n*getfact(n-1)
 n=int(input("Enter the number: "))
 print (getfact(n))
+'''
+
+def getfact(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+n = int(input("Enter the number: "))
+print(getfact(n))
