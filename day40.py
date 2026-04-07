3a3ee0d0b845c9d69a623d5e10a3827ad958cb6d
Bhumika Shinde <bhumikashinde42@gmail.com>
2026-04-07 09:38:00 +0530

    Find non-repeating elements in an array Python

---
+++ day40.py
@@ -0,0 +1,32 @@
+'''
+def find_non_repeating(arr):
+    n = len(arr)
+
+    for i in range(n):
+        count = 0
+        for j in range(n):
+            if arr[i] == arr[j]:
+                count += 1
+
+        if count == 1:
+            print(arr[i])
+
+# Driver code
+arr = [10, 20, 30, 40, 10, 20, 50, 10]
+find_non_repeating(arr)
+'''
+def count(arr, n):
+    mp = {}
+
+    for i in range(n):
+        if arr[i] in mp:
+            mp[arr[i]] += 1
+        else:
+            mp[arr[i]] = 1
+
+    for x in mp:
+        if mp[x] == 1:
+            print(x)    
+
+arr = [10, 30, 40, 20, 10, 20, 50, 10]
+count(arr, len(arr))
