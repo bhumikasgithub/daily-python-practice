a3370d71b1539fcf88723b21b194e40c4a9a4bc0
Bhumika Shinde <bhumikashinde42@gmail.com>
2026-04-10 09:12:30 +0530

    Python Program to Find Maximum Scalar Product of Two Vectors in an Array

--- day43.py
+++ day43.py
@@ -1,6 +1,6 @@
 #pyhton program to find the maximium scaler product of the two arrya
 
-arr1 = [1, 2, 6, 3, 7]
+'''arr1 = [1, 2, 6, 3, 7]
 arr2 = [10, 7, 45, 3, 7]
 n=len(arr1)
 for i in range(n):
@@ -11,6 +11,16 @@ for i in range(n):
     for j in range(i+1,n):
         if arr2[i]>arr2[j]:
             arr2[i],arr2[j] = arr2[j],arr2[i]
+product = 0
+for i in range(n):
+    product += arr1[i]*arr2[i]
+print(product)
+'''
+arr1= [1, 2, 3]
+arr2=[4, 5, 6]
+arr1.sort()
+arr2.sort()
+n=len(arr1)
 product = 0
 for i in range(n):
     product += arr1[i]*arr2[i]
