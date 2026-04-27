65bcbd325d78c32fb6bc86047f59e7224118e7a4
Bhumika Shinde <bhumikashinde42@gmail.com>
2026-04-27 10:29:49 +0530

    Python program for rotation of elements of array- left and right

---
+++ day55.py
@@ -0,0 +1,16 @@
+#python program to rotaed the array
+def leftrotate(arr,d,n):
+    for i in range(d):
+        leftrotatebyone(arr,n)
+  
+def leftrotatebyone(arr,n):
+    temp=arr[0]
+    for i in range(n-1):
+        arr[i]=arr[i+1]
+    arr[n-1]=temp
+def printarray(arr,size):
+    for i in range(size):
+        print("%d " % arr[i], end=" ")
+arr=[1,2,3,4,5,6,7]
+leftrotate(arr,3,7)
+printarray(arr,7)
