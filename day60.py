7b1c6b1152cd65331bd0ce80371876578ad3d167
Bhumika Shinde <bhumikashinde42@gmail.com>
2026-05-03 11:04:26 +0530

    Python Program for checking a character is a vowel or consonant

---
+++ day60.py
@@ -0,0 +1,15 @@
+# python program to find charecter is vowel or consonnat
+
+Vowel= ["a","e","i","o","u"]
+u = ["A","E","I","O","U"]
+
+word= input("enter the string:  ")
+
+if word in Vowel or word in u:
+    print("word is vowel")
+else:
+    print("word is not vowel")
+    
+
+
+
