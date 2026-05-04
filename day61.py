7b62321c2177630ad9e7329fea64a2ec8f11b253
Bhumika Shinde <bhumikashinde42@gmail.com>
2026-05-04 11:24:49 +0530

    Python program to check whether a character is an alphabet or not

--- day61.py
+++ day61.py
@@ -2,13 +2,13 @@
 
 
 ch = input("enter the charecture: ")
-'''
+
 if 'A' <= ch <= 'Z' or 'a' <= ch <= 'z':
     print("it is alphabet")
 else:    
     print("it is not alphabet")    
 
-'''
+
 
 if 96 < ord(ch) < 123 or 64 < ord(ch) < 91:
     print("it is alphabet")
