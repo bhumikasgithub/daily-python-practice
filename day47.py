ef2eddda2a1907f3b32d4f54e1400c563858c911
Bhumika Shinde <bhumikashinde42@gmail.com>
2026-05-09 17:13:59 +0530

    python program to find the maximum sum of the contigous subarray

--- day47.py
+++ day47.py
@@ -1,11 +1,11 @@
-#python program to find the maximum sum of the contigous subarray
+python program to find the maximum sum of the contigous subarray
 
 def maxsumofarray(arr,n):
     result=arr[0]
     for i in range(n):
         mul=arr[i]
         for j in range(i+1,n):
-            mul*=arr[j]
+            mul*=arr[j] 
             result= max(result,mul)
 
         result=max(result,mul)
